094921833276110c325b05dd63b98f7bb84df643
chih860121-dev <chih860121@gmail.com>
2026-07-01 10:15:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='pf-v3';
+const C='pf-v4';
 const ASSETS=['./','./index.html','./icon.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
